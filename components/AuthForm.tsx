ba7ad5381fa864cd5a5a9ad6432c74e795b44a95
konef <k.one.for.all.k@gmail.com>
2026-03-08 22:46:06 +0900

    fix todo type module

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -40,7 +40,7 @@ export const AuthForm = ({
             onClick={onSignUp}
             className="rounded bg-blue-600 px-4 py-2 text-white"
           >
-            サインアップ
+            新規登録
           </button>
           <button
             onClick={onSignIn}
